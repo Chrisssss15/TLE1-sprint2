538dd26d9ccadfffa6916108ccec0d9adcbfee0d
Chris Tang <1073412@hr.nl>
2024-09-24 12:10:37 +0200

    vergeten te committen maar header af, ben nu bezig met footer

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() { // wacht tot de pagina geladen is
+    const headerElement = document.getElementById('header'); // geef de header een element
+    if (headerElement) { // als de header bestaat
+        fetch('header.html') // haal de header op
+            .then(response => response.text()) // zet de response om naar tekst
+            .then(html => { // zet de tekst om naar html
+                headerElement.innerHTML = html; // zet de html in de header
+            })
+            .catch(error => console.error('Error loading the header:', error)); // als er een error is, toon deze
+    } else {
+        console.error('The header element does not exist!'); // als de header niet bestaat, toon dit
+    }
+});
