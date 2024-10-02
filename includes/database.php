9a3d833a93e664a039eb45ec0c536f673d4842b2
Yosei10200 <yosei.saito@gmail.com>
2024-10-02 10:08:37 +0200

    Database

--- includes/database.php
+++ includes/database.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "TLE1-team5-2";
+$host = "stud.hosted.hr.nl";
+$user = "prj_2024_2025_tle1_exp_t5";
+$password = "ovohquei";
+$database = "prj_2024_2025_tle1_exp_t5";
 
 $db = mysqli_connect($host, $user, $password, $database)
 or die("Error: " . mysqli_connect_error());
